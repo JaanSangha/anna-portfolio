40cb5d5dd737d0a3072f62aa494e1b4d100d9299
jaan <jaansangha10@gmail.com>
2023-11-09 12:45:15 -0500

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,11 @@ function Navbar() {
             <div class="container">
                 <div class="d-flex justify-content-center py-5 col-12">
                     <ul class="nav nav-pills">
-                        <li class="nav-item"><a href="#top" class="nav-link" aria-current="page">Home</a></li>
-                        <li class="nav-item"><a href="#top" class="nav-link">About</a></li>
-                        <li class="nav-item"><a href="#top" class="nav-link">Upcoming</a></li>
-                        <li class="nav-item"><a href="#top" class="nav-link">Set List</a></li>
-                        <li class="nav-item"><a href="#top" class="nav-link">Contact</a></li>
+                        <li class="nav-item border-end border-start"><a href="#top" class="nav-link" aria-current="page">Home</a></li>
+                        <li class="nav-item border-end"><a href="#top" class="nav-link">About</a></li>
+                        <li class="nav-item border-end"><a href="#top" class="nav-link">Upcoming</a></li>
+                        <li class="nav-item border-end"><a href="#top" class="nav-link">Set List</a></li>
+                        <li class="nav-item border-end"><a href="#top" class="nav-link">Contact</a></li>
                     </ul>
                 </div>
             </div>
