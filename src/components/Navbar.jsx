8837f6482d613dac1e516e62938fdc52ec3a5bd3
jaan <jaansangha10@gmail.com>
2023-11-10 22:19:23 -0500

    adjusted navbar format

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,13 @@ import React from "react";
 
 function Navbar() {
     return(
-        <div className="navbar">
-            <div class="container">
-                <div class="d-flex justify-content-center py-5 col-12">
+        <div className="navbar d-flex justify-content-center py-5 col-12">
                     <ul class="nav nav-pills">
-                        <li class="nav-item border-end border-start"><a href="#top" class="nav-link" aria-current="page">Home</a></li>
                         <li class="nav-item border-end"><a href="#top" class="nav-link">About</a></li>
                         <li class="nav-item border-end"><a href="#top" class="nav-link">Upcoming</a></li>
                         <li class="nav-item border-end"><a href="#top" class="nav-link">Set List</a></li>
                         <li class="nav-item border-end"><a href="#top" class="nav-link">Contact</a></li>
                     </ul>
-                </div>
-            </div>
         </div>
     );
 }
